56c263499f41bc8b80a66d1f26dac3f208e4361d
Padmasana2 <khalil.aboumalih@outlook.com>
2023-06-22 17:14:46 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,8 +10,8 @@ if (size <= 0)
 {
 _putchar('\n');
 }
-else {
-
+else
+{
 int i, j;
 for (i = 0; i < size; i++)
 {
@@ -20,6 +20,7 @@ for (j = 0; j < size; j++)
 _putchar('#');
 }
 _putchar('\n');
+}
 }
 }
 }
